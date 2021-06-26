69fcca1f9d6789bd30ef5e426136c7382046aac3
Marcos RC <mrcmarcos100@gmail.com>
2021-06-26 14:37:44 -0300

    Finish class #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import { AuthProvider } from "./contexts/AuthContext";
+
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
 
 import './styles/global.scss'
 
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Route path='/' exact component={Home} />
-        <Route path='/rooms/new' component={NewRoom} />
-      </AuthProvider>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <AuthProvider>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/rooms/new' component={NewRoom} />
+            <Route path='/rooms/:id' component={Room} />
+          </Switch>
+        </AuthProvider>
+      </BrowserRouter>
+      <Toaster />
+    </>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,21 @@
+import { useState, FormEvent } from 'react'
 import { useHistory } from 'react-router-dom'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
-import { Button } from '../components/Button'
-import { auth, firebase } from '../services/firebase'
-
-import '../styles/auth.scss'
+import { database } from '../services/firebase'
 import { useAuth } from '../contexts/AuthContext'
 
+import { Button } from '../components/Button'
+
+import '../styles/auth.scss'
+import toast from 'react-hot-toast'
+
 export function Home() {
+  const [roomCode, setRoomCode] = useState('')
+
   const history = useHistory()
   const { user, signInWithGoogle } = useAuth()
 
@@ -20,6 +25,21 @@ export function Home() {
     }
 
     history.push('/rooms/new')
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') return
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+    if (!roomRef.exists()) {
+      toast.error('Room does not exists.')
+      return
+    }
+
+    history.push(`/rooms/${roomCode}`)
   }
 
   return (
@@ -37,8 +57,13 @@ export function Home() {
             Crie sua sala com o Goggle
           </button>
           <div className='separator'>ou entre em uma sala</div>
-          <form>
-            <input type="text" placeholder='Digite o cógido da sala' />
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder='Digite o cógido da sala'
+              onChange={e => setRoomCode(e.target.value)}
+              value={roomCode}
+            />
             <Button type="submit">
               Entrar na sala
             </Button>
